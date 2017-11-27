45d5c4476f86b28309456680629985c464770a5a
Tran Ngoc Son <tranngocson12d@gmail.com>
2017-11-27 22:46:55 +0700

    Update OrderDetailsController.php

--- customer-website/app/Controller/OrderDetailsController.php
+++ customer-website/app/Controller/OrderDetailsController.php
@@ -1,15 +1,15 @@
 <?php
 App::uses('AppController', 'Controller');
-// AuthComponent::user('id');
+AuthComponent::user('id');
 class OrderDetailsController extends AppController {
   var $helpers = array('Paginator','Html');
   var $paginate = array();
   public $uses =array('Orderdetail','Account','Order');
   public $name = "OrderDetails";
     function index(){
-      // $account_id = $this->Auth->user('id');
-        $account_id = $this->Account->find('first',array('Account.id'=>'id'));
-        $conditions = array('Order.account_id' =>$account_id['Account']['id']);
+         $account_id = $this->Auth->user('id');
+        //$account_id = $this->Account->find('first',array('Account.id'=>'id'));
+        $conditions = array('Order.account_id' =>$account_id);
         $allOrder = $this->Order->find('all',array('conditions'=>$conditions));
         $arr =[];
         foreach ($allOrder as $order){
