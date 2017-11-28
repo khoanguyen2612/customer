4acebb38e90e8c1525d8697850d59adf704cfd83
ngocson <tranngocson12d@gmail.com>
2017-11-28 13:47:48 +0700

    update

--- customer-website/app/Controller/OrderHistorysController.php
+++ customer-website/app/Controller/OrderHistorysController.php
@@ -1,25 +1,17 @@
 <?php
 App::uses('AppController', 'Controller');
 AuthComponent::user('id');
-<<<<<<< HEAD:customer-website/app/Controller/OrderHistorysController.php
 class OrderHistorysController extends AppController {
-=======
-class OrderDetailsController extends AppController {
->>>>>>> 8cce337546c4a42d96cdd4303f7e86fe8f735a70:customer-website/app/Controller/OrderDetailsController.php
   var $helpers = array('Paginator','Html');
   var $paginate = array();
   public $uses =array('Orderdetail','Account','Order');
   public $name = "OrderHistorys";
     function index(){
         $account_id = $this->Auth->user('id');
-<<<<<<< HEAD:customer-website/app/Controller/OrderHistorysController.php
         $conditions = array('Order.account_id' =>$account_id);
         // $account_id = $this->Account->find('first',array('Account.id'=>'id'));
         // $conditions = array('Order.account_id' =>$account_id['Account']['id']);
-=======
-        //$account_id = $this->Account->find('first',array('Account.id'=>'id'));
         $conditions = array('Order.account_id' =>$account_id);
->>>>>>> 8cce337546c4a42d96cdd4303f7e86fe8f735a70:customer-website/app/Controller/OrderDetailsController.php
         $allOrder = $this->Order->find('all',array('conditions'=>$conditions));
         $arr =[];
         foreach ($allOrder as $order){
