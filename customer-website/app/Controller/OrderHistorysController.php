c4eac6d692f553934a08faf8e9da1923a92a5ebd
ngocson <tranngocson12d@gmail.com>
2017-11-28 15:59:42 +0700

    done

--- customer-website/app/Controller/OrderHistorysController.php
+++ customer-website/app/Controller/OrderHistorysController.php
@@ -2,12 +2,14 @@
 App::uses('AppController', 'Controller');
 class OrderHistorysController extends AppController {
   var $helpers = array('Paginator','Html');
+  var $paginate = array();
   public $uses =array('Orderdetail','Account','Order');
   public $name = "OrderHistorys";
     function index(){
         $account_id = $this->Auth->user('id');
+        //$account_id = $this->Account->find('first',array('Account.id'=>'id'));
         $conditions = array('Order.account_id' =>$account_id);
-         $allOrder = $this->Order->find('all',array('conditions'=>$conditions));
+        $allOrder = $this->Order->find('all',array('conditions'=>$conditions));
         $arr =[];
         foreach ($allOrder as $order){
             $order_id = $order['Order']['id'];
@@ -15,13 +17,11 @@ class OrderHistorysController extends AppController {
             $allOrderDetail = $this->Orderdetail->find('all',array('conditions'=>$conditions));
             $arr[] = $allOrderDetail;
         }
-        $dem = count($allOrderDetail);
-        $arr = array('Account' => array(
+        //$dem = count($allOrderDetail);
+         $arr = array('Account' => array(
           'Account.id'=>$account_id['Account']['id'],
           'Account.credit'=>$account_id['Account']['credit']),
-          'Orderdetail'=>$allOrderDetail);
-       
+          'Orderdetail'=>$allOrderDetail);      
         $this->set('data',$arr);                  
     }
 }
-
